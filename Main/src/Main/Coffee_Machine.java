ad739795d41a2cf1b65ae005420f2f48861d0c31
GayaneGulanyan <66916093+GayaneGulanyan@users.noreply.github.com>
2020-07-11 17:11:24 +0400

    Code changed: Added getCups for cups numbers reading, setIngredients to calculate ingredients volume and getIngredients to provide information to user about the ingredients necessary volume.

--- Main/src/Main/Coffee_Machine.java
+++ Main/src/Main/Coffee_Machine.java
@@ -1,14 +1,31 @@
 package Main;
+import java.util.Scanner;
 
 public class Coffee_Machine {
-    public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
 
-    }
+    public static void main(String[] args) {
+        setIngredients(getCups());
+        }
+    public static int getCups() {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Write how many cups of coffee you will need...");
+            int cups = Integer.parseInt(in.nextLine());
+            System.out.println("For " + cups + " cups of coffee you will need:");
+            in.close();
+            return cups;
+        }
+    public static void setIngredients(int cups) {
+        int water = 200;
+        int milk = 50;
+        int coffeeBeans = 15;
+        water = cups * water;
+        milk = cups * milk;
+        coffeeBeans = cups * coffeeBeans;
+        getIngredients(water, milk, coffeeBeans);
+        }
+    public static void getIngredients(int water, int milk, int coffeeBeans){
+        System.out.println(water + " ml of water");
+        System.out.println(milk + " ml of milk");
+        System.out.println(coffeeBeans + " gr of coffee beans");
+        }
 }
